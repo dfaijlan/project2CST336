435d0e36e660956e79299d276b4186eaec52992a
Dominic Fajilan <dfajilan@csumb.edu>
2017-11-14 23:02:35 +0000

    First commit

---
+++ movie.php
@@ -0,0 +1,188 @@
+<?php
+session_start();
+
+//Using Antonio's database
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$username = "bfeaad637110cb";
+$password = "c0419d9c";
+$dbname = "heroku_303da836d19345a"; 
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_GET["submitButton"])) {
+    $movie_name = $_GET["movie_name"];
+    $movie_price = $_GET["movie_price"];
+    $release_year = $_GET["movie_release_year"];
+    
+    $sql = "SELECT *
+            FROM movie
+            WHERE 1";
+            
+    if (isset($_GET["filter_by_name_radio"])) {
+        $sql = $sql . " AND movie_name = '$movie_name'";
+        if ($movie_name == "") {
+            echo "<br>Please enter a movie name<br>";
+        }
+    }
+    if (isset($_GET["filter_by_price_radio"])) {
+        $sql = $sql . " AND movie_price = '$movie_price'";
+        if ($movie_price == "") {
+            echo "<br>Please enter a movie price to search<br>";
+        }
+    }
+    if (isset($_GET["filter_by_release_year_radio"])) {
+        $sql = $sql . " AND release_year = '$release_year'";
+        if ($release_year == "") {
+            echo "<br>Please enter a release year to search<br>";
+        }
+    }
+    if (isset($_GET["sort_by_option"])) {
+        $value = $_GET["sort_by_option"];
+        if ($value != "") {
+            $sql = $sql . " ORDER BY $value";
+        }
+    }
+    if (isset($_GET["order_radio"])) {
+        $value = $_GET["order_radio"];
+        if ($_GET["order_radio"] == "descending") {
+            $sql = $sql . " desc";
+        }
+    }
+    $result = $conn->query($sql);
+    
+    echo "<table>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Year</th>
+            </tr>";
+            
+    if ($result->num_rows > 0) {
+        //output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>" .$row["movie_name"]. "</td><td>".$row["movie_price"]. "</td><td> ".$row["release_year"] ."</td><td>";
+            echo "[<a href='add_to_cart.php?name=" . $row['movie_name'] . "&location=movie'> Add to cart </a>]</td></tr>"; 
+        }
+    }
+    echo "</table>";
+}
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Movie</title>
+        <link rel ="stylesheet" href="/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Bungee+Shade" rel="stylesheet">
+        <h1 id="mfont">Movie</h1>
+    </head>
+    <body>
+        <div id="nav">
+    <div id="nav_wrapper">
+        <ul>
+            <li><a href="/index.php">Home</a>
+            </li>
+            <li> <a href="#">Store Department</a>
+
+                <ul>
+                    <li><a href="/anime.php">Anime</a>
+                    </li>
+                    <li><a href="/video_game.php">Video Games</a>
+                    </li>
+                    <li><a href="/shopping_cart.php">Cart</a>
+                    </li>
+                </ul>
+            </li>
+            <li> 
+                <a href="#">Movie Titles</a>
+                 <ul>
+                       <li><a href = "https://www.rottentomatoes.com/m/kung_fu_panda/">Kung Fu Panda.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/bee_movie/">Bee Movie.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/avatar/">Avatar.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/moana_2016/">Moana.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/zootopia/">Zootopia.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/frozen_2013/">Frozen.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/trolls/">Trolls.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/the_emoji_movie/">Emoji Movie.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/mulan/">Mulan.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/toy_story/">Toy Story.</a>
+                       <li><a href = "https://www.rottentomatoes.com/m/toy_story_2/">Toy Story 2.</a>
+                 </ul>
+         </li>
+        </ul>
+    </div>
+    <!-- Nav wrapper end -->
+</div>
+<!-- Nav end -->
+        <form id="mfont">
+            <fieldset id="filter_by_name">
+                Filter by Name:
+                <input type="Radio" name="filter_by_name_radio" value="name">
+                Name:
+                <input type="text" name="movie_name">
+            </fieldset>
+            <fieldset id="filter_by_price">
+                Filter by Price:
+                <input type="Radio" name="filter_by_price_radio" value="price">
+                Price (please include $ before price):
+                <input type="text" name="movie_price">
+            </fieldset>
+            <fieldset id="filter_by_release_year">
+                Filter by Release Year:
+                <input type="Radio" name="filter_by_release_year_radio" value="release_year">
+                Release Year:
+                <input type="text" name="movie_release_year">
+            </fieldset>
+            <!--    <fieldset id="item_info">-->
+            <!--    Item Info:-->
+            <!--      <form name="movie">-->
+            <!--            <select name="movie" id="movieMenu">-->
+            <!--                <option value = "nothing" selected ="selected">Select a Film</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/kung_fu_panda/">Kung Fu Panda.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/bee_movie/">Bee Movie.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/avatar/">Avatar.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/moana_2016/">Moana.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/zootopia/">Zootopia.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/frozen_2013/">Frozen.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/trolls/">Trolls.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/the_emoji_movie/">Emoji Movie.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/mulan/">Mulan.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/toy_story/">Toy Story.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/toy_story_2/">Toy Story 2.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/toy_story_3/">Toy Story 3.</option>-->
+            <!--                <option value = "https://www.rottentomatoes.com/m/antman/">Ant Man.</option>-->
+            <!--            </select>-->
+            <!--        </form>-->
+            <!--        <script type="text/javascript">-->
+            <!--            var urlMenu = document.getElementById('movieMenu');-->
+            <!--            urlMenu.onchange = function(){-->
+            <!--                var userOption = this.options[this.selectedIndex];-->
+            <!--                if(userOption.value !="nothing"){-->
+            <!--                    window.open(userOption.value, "Movies Page", "");-->
+            <!--                }-->
+            <!--            }-->
+            <!--        </script>-->
+            <!--</fieldset>-->
+            <fieldset id="sort_field">
+                Order results by:
+                <select name="sort_by_option">
+                    <option value="">Select...</option>
+                    <option value="movie_name">Name</option>
+                    <option value="movie_price">Price</option>
+                    <option value="release_year">Release Year</option>
+                </select>
+                <input type="Radio" name="order_radio" value="ascending">Ascending
+                <input type="Radio" name="order_radio" value="descending">Descending
+            </fieldset>
+            <fieldset id="submitButton">
+                <input type="submit" id="submit" name="submitButton" value="Submit" />
+            </fieldset>
+            
+        </form>
+    </body>
+</html>
---
+++ video_game.php
@@ -0,0 +1,202 @@
+<?php
+session_start();
+
+//Using Antonio's database
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$username = "bfeaad637110cb";
+$password = "c0419d9c";
+$dbname = "heroku_303da836d19345a"; 
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_GET["submitButton"])) {
+    $video_game_name = $_GET["video_game_name"];
+    $video_game_price = $_GET["video_game_price"];
+    $release_year = $_GET["video_game_release_year"];
+    
+    $sql = "SELECT *
+            FROM video_game
+            WHERE 1";
+            
+    if (isset($_GET["filter_by_name_radio"])) {
+        $sql = $sql . " AND video_game_name = '$video_game_name'";
+        if ($video_game_name == "") {
+            echo "<br>Please enter a video game name<br>";
+        }
+    }
+    if (isset($_GET["filter_by_price_radio"])) {
+        $sql = $sql . " AND video_game_price = '$video_game_price'";
+        if ($video_game_price == "") {
+            echo "<br>Please enter a video game price to search<br>";
+        }
+    }
+    if (isset($_GET["filter_by_release_year_radio"])) {
+        $sql = $sql . " AND release_year = '$release_year'";
+        if ($release_year == "") {
+            echo "<br>Please enter a release year to search<br>";
+        }
+    }
+    if (isset($_GET["sort_by_option"])) {
+        $value = $_GET["sort_by_option"];
+        if ($value != "") {
+            $sql = $sql . " ORDER BY $value";
+        }
+    }
+    if (isset($_GET["order_radio"])) {
+        $value = $_GET["order_radio"];
+        if ($_GET["order_radio"] == "descending") {
+            $sql = $sql . " desc";
+        }
+    }
+    $result = $conn->query($sql);
+    
+    echo "<table>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Year</th>
+            </tr>";
+            
+    if ($result->num_rows > 0) {
+        //output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>" .$row["video_game_name"]. "</td><td>".$row["video_game_price"]. "</td><td> ".$row["release_year"] ."</td><td>";
+            echo "[<a href='add_to_cart.php?name=" . $row['video_game_name'] . "&location=video_game'> Add to cart </a>]</td></tr>"; 
+        }
+    }
+    echo "</table>";
+}
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Video Games</title>
+        <link rel ="stylesheet" href="/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+        <h1 id="vgames">Video Games</h1>
+    </head>
+    <body>
+        <div id="nav">
+    <div id="nav_wrapper">
+        <ul>
+            <li><a href="/index.php">Home</a>
+            </li>
+            <li> <a href="#">Store Department</a>
+
+                <ul>
+                    <li><a href="/anime.php">Anime</a>
+                    </li>
+                    <li><a href="/movie.php">Movies</a>
+                    </li>
+                    <li><a href="/shopping_cart.php">Cart</a>
+                    </li>
+                </ul>
+                <li> 
+                <a href="#">Video Game Titles</a>
+                 <ul>
+                       
+                            <li><a href  = "http://www.ign.com/games/battle-fantasia/xbox-360-14246645/">Batlle Fantasia.</a>
+                            <li><a href  = "http://www.ign.com/games/cuphead/">Cuphead.</a>
+                            <li><a href = "http://www.ign.com/games/dungeon-fighter-online/">DFO.</a>
+                            <li><a href = "http://www.ign.com/games/kingdom-hearts/ps2-16467/">Kingdom Hearts.</a>
+                            <li><a href = "http://www.ign.com/games/kingdom-hearts-ii/ps2-550308/">Kingdom Hearts 2.</a>
+                            <li><a href = "http://www.ign.com/games/league-of-legends/">League of Legends.</a>
+                            <li><a href = "http://www.ign.com/games/lost-saga-167231/">Lost Saga.</a>
+                            <li><a href = "http://maplestory.nexon.net/landing/">Maplestory.</a>
+                            <li><a href = "http://www.ign.com/articles/1999/10/08/marvel-vs-capcom-clash-of-super-heroes-review/">Marvel vs Capcom.</a>
+                            <li><a href = "http://www.ign.com/articles/2017/09/18/marvel-vs-capcom-infinite-review/">Marvel vs Capcom Infite Wars.</a>
+                            <li><a href = "http://www.ign.com/games/mega-man-battle-network/gba-15252/">Megaman Battle Network.</a>
+                            <li><a href = "http://www.ign.com/games/mega-man-x/snes-6872/">Megaman X.</a>
+                            <li><a href = "http://www.ign.com/games/mega-man-x4/ps-2083/">Megaman X4.</a>
+                            <li><a href = "http://www.ign.com/games/mega-man-x8/">Megaman X8.</a>
+                            <li><a href = "http://www.ign.com/games/mega-man-zx/">Megaman ZX Adventures.</a>
+                            <li><a href = "http://www.ign.com/games/super-smash-bros/n64-10494/">Super Smash Bros.</a>
+                            <li><a href = "http://www.ign.com/games/super-smash-bros-melee/">Super Smash Bros Melee.</a>
+            <               <li><a href = "http://www.ign.com/games/the-world-ends-with-you/">The World Ends With You.</a>
+                 </ul>
+            </li>
+        </ul>
+    </div>
+    <!-- Nav wrapper end -->
+</div>
+<!-- Nav end -->
+        <form id="vgames">
+            <fieldset id="filter_by_name">
+                Filter by Name:
+                <input type="Radio" name="filter_by_name_radio" value="name">
+                Name:
+                <input type="text" name="video_game_name">
+            </fieldset>
+            <fieldset id="filter_by_price">
+                Filter by Price:
+                <input type="Radio" name="filter_by_price_radio" value="price">
+                Price (please include $ before price):
+                <input type="text" name="video_game_price">
+            </fieldset>
+            <fieldset id="filter_by_release_year">
+                Filter by Release Year:
+                <input type="Radio" name="filter_by_release_year_radio" value="release_year">
+                Release Year:
+                <input type="text" name="video_game_release_year">
+            </fieldset>
+            <!--<fieldset id="item_info">-->
+            <!--    Item Info:-->
+            <!--      <form name="games">-->
+            <!--            <select name="games" id="gamesMenu">-->
+            <!--                <option value = "nothing" selected ="selected">Select a game</option>-->
+            <!--                <option value = "http://www.ign.com/games/battle-fantasia/xbox-360-14246645/">Batlle Fantasia.</option>-->
+            <!--                <option value = "http://www.ign.com/games/cuphead/">Cuphead.</option>-->
+            <!--                <option value = "http://www.ign.com/games/dungeon-fighter-online/">DFO.</option>-->
+            <!--                <option value = "http://www.ign.com/games/kingdom-hearts/ps2-16467/">Kingdom Hearts.</option>-->
+            <!--                <option value = "http://www.ign.com/games/kingdom-hearts-ii/ps2-550308/">Kingdom Hearts 2.</option>-->
+            <!--                <option value = "http://www.ign.com/games/league-of-legends/">League of Legends.</option>-->
+            <!--                <option value = "http://www.ign.com/games/lost-saga-167231/">Lost Saga.</option>-->
+            <!--                <option value = "http://maplestory.nexon.net/landing/">Maplestory.</option>-->
+            <!--                <option value = "http://www.ign.com/articles/1999/10/08/marvel-vs-capcom-clash-of-super-heroes-review/">Marvel vs Capcom.</option>-->
+            <!--                <option value = "http://www.ign.com/articles/2017/09/18/marvel-vs-capcom-infinite-review/">Marvel vs Capcom Infite Wars.</option>-->
+            <!--                <option value = "http://www.ign.com/games/mega-man-battle-network/gba-15252/">Megaman Battle Network.</option>-->
+            <!--                <option value = "http://www.ign.com/games/mega-man-x/snes-6872/">Megaman X.</option>-->
+            <!--                <option value = "http://www.ign.com/games/mega-man-x4/ps-2083/">Megaman X4.</option>-->
+            <!--                <option value = "http://www.ign.com/games/mega-man-x8/">Megaman X8.</option>-->
+            <!--                <option value = "http://www.ign.com/games/mega-man-zx/">Megaman ZX Adventures.</option>-->
+            <!--                <option value = "http://www.ign.com/games/super-smash-bros/n64-10494/">Super Smash Bros.</option>-->
+            <!--                <option value = "http://www.ign.com/games/super-smash-bros-melee/">Super Smash Bros Melee.</option>-->
+            <!--                <option value = "http://www.ign.com/games/the-world-ends-with-you/">The World Ends With You.</option>-->
+            <!--            </select>-->
+            <!--        </form>-->
+                    
+            <!--        <script type="text/javascript">-->
+            <!--            var urlMenu = document.getElementById('gamesMenu');-->
+            <!--            urlMenu.onchange = function(){-->
+            <!--                var userOption = this.options[this.selectedIndex];-->
+            <!--                if(userOption.value !="nothing"){-->
+            <!--                    window.open(userOption.value, "Videogames Page", "");-->
+            <!--                }-->
+            <!--            }-->
+                        
+            <!--        </script>-->
+            <!--</fieldset>-->
+            <fieldset id="sort_field">
+                Order results by:
+                <select name="sort_by_option">
+                    <option value="">Select...</option>
+                    <option value="video_game_name">Name</option>
+                    <option value="video_game_price">Price</option>
+                    <option value="release_year">Release Year</option>
+                </select>
+                <input type="Radio" name="order_radio" value="ascending">Ascending
+                <input type="Radio" name="order_radio" value="descending">Descending
+            </fieldset>
+            <fieldset id="submitButton">
+                <input type="submit" id="submit" name="submitButton" value="Submit" />
+            </fieldset>
+            
+        </form>
+    </body>
+</html>
