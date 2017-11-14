6a7d214d69340e5c2a1de5941f185dacd8210da5
antonioVgomez <avillagomez@csumb.edu>
2017-11-14 11:15:23 -0800

    Update anime.php

--- anime.php
+++ anime.php
@@ -65,35 +65,11 @@ if(isset($_GET["submitButton"])) {
 ?>
 <html>
     <head>
-        <meta charset="utf-8">
         <title>Anime</title>
-        <link rel ="stylesheet" href="/style.css">
-        <link href="https://fonts.googleapis.com/css?family=Ewert" rel="stylesheet">
-        <h1 id="aFont">Anime</h1>
+        <h1>Anime</h1>
     </head>
-   <body>
-        <div id="nav">
-    <div id="nav_wrapper">
-        <ul>
-            <li><a href="/index.php">Home</a>
-            </li>
-            <li> <a href="#">Store Department</a>
-
-                <ul>
-                    <li><a href="/movie.php">Movies</a>
-                    </li>
-                    <li><a href="/video_game.php">Video Games</a>
-                    </li>
-                    <li><a href="/clear_cart.php">Cart</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-    <!-- Nav wrapper end -->
-</div>
-<!-- Nav end -->
-        <form id="aFont">
+    <body>
+        <form>
             <fieldset id="filter_by_name">
                 Filter by Name:
                 <input type="Radio" name="filter_by_name_radio" value="name">
@@ -111,37 +87,6 @@ if(isset($_GET["submitButton"])) {
                 <input type="Radio" name="filter_by_release_year_radio" value="release_year">
                 Release Year:
                 <input type="text" name="anime_release_year">
-            </fieldset>
-            <fieldset id="item_info">
-                Item Info:
-                  <form name="Anime">
-                        <select name="Anime" id="animeMenu">
-                            <option value = "nothing" selected ="selected">Select an Anime</option>
-                            <option value = "https://myanimelist.net/anime/30276/One_Punch_Man/">One Punch Man.</option>
-                            <option value = "https://myanimelist.net/manga/1517/JoJo_no_Kimyou_na_Bouken_Part_1__Phantom_Blood/">JJBA Part 1.</option>
-                            <option value = "https://myanimelist.net/manga/1630/JoJo_no_Kimyou_na_Bouken_Part_2__Sentou_Chuuryuu/">JJBA Part 2.</option>
-                            <option value = "https://myanimelist.net/manga/872/JoJo_no_Kimyou_na_Bouken_Part_3__Stardust_Crusaders/">JJBA Part 3.</option>
-                            <option value = "https://myanimelist.net/manga/3006/JoJo_no_Kimyou_na_Bouken_Part_4__Diamond_wa_Kudakenai/">JJBA Part 4.</option>
-                            <option value = "https://myanimelist.net/manga/75989/Boku_no_Hero_Academia/">My Hero Academia Sea.</option>
-                            <option value = "https://myanimelist.net/anime/35262/Boku_no_Hero_Academia_2nd_Season__Hero_Note/">My Hero Academia Sea 2.</option>
-                            <option value = "https://myanimelist.net/manga/53/Yuu%E2%98%86Yuu%E2%98%86Hakusho/">Yu Yu Hakusho.</option>
-                            <option value = "https://myanimelist.net/anime/1604/Katekyo_Hitman_Reborn?q=hitman%20re/">Hitman Reborn.</option>
-                            <option value = "https://myanimelist.net/anime/20/Naruto/">Naruto.</option>
-                            
-                     
-                        </select>
-                    </form>
-                    
-                    <script type="text/javascript">
-                        var urlMenu = document.getElementById('animeMenu');
-                        urlMenu.onchange = function(){
-                            var userOption = this.options[this.selectedIndex];
-                            if(userOption.value !="nothing"){
-                                window.open(userOption.value, "Anime Page", "");
-                            }
-                        }
-                        
-                    </script>
             </fieldset>
             <fieldset id="sort_field">
                 Order results by:
