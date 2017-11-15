915382ae4997a2b23819782ddac48c3598fdfe9b
Dominic Fajilan <dfajilan@csumb.edu>
2017-11-15 01:29:25 +0000

    Added nav bar to homepage

--- index.php
+++ index.php
@@ -45,10 +45,37 @@ if (isset($_GET["submitButton"])) {
     <head>
         <title>Online Catalog</title>
         <h1>Online Catalog</h1>
+        <style>
+            @import url("style.css");
+        </style>
     </head>
     <body>
-        
+             <div id="nav">
+    <div id="nav_wrapper">
+        <ul>
+            <li><a href="/index.php">Home</a>
+            </li>
+            <li> <a href="#">Store Department</a>
+
+                <ul>
+                    <li><a href="/anime.php">Anime</a>
+                    </li>
+                    <li><a href="/movie.php">Movies</a>
+                    </li>
+                    <li><a href="/video_game.php">Video Games</a>
+                    </li>
+                </ul>
+            </li>
+             <li> 
+                <a href="shopping_cart.php">Shopping Cart</a>
+         </li>
+        </ul>
+    </div>
+    <!-- Nav wrapper end -->
+</div>
+<!-- Nav end -->
         <form>
+            <br>
             <fieldset id="search_options">
                 Search for:<br>
                 <input type="Radio" name="search_options" value="anime">Anime<br>
@@ -58,12 +85,6 @@ if (isset($_GET["submitButton"])) {
         <fieldset id="submitButton">
             <input type="submit" id="submit" name="submitButton" value="Submit" />
         </fieldset>
-        
-        </form>
-        <form action="shopping_cart.php">
-                
-                <input type="submit" id="mainPageButtons" value="View Shopping cart" />
-                
         </form>
     </body>
 </html>
