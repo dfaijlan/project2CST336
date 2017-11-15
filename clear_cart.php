6fb383a18deee751078d36acbcf70b71b06288b0
antonioVgomez <avillagomez@csumb.edu>
2017-11-14 22:04:23 -0800

    Update clear_cart.php

--- clear_cart.php
+++ clear_cart.php
@@ -1,13 +1,6 @@
 <?php
 session_start();
-<<<<<<< HEAD
 //Kills the session which in turn clears all session variables that the cart uses.
 session_destroy();
-=======
-
-//Kills the session which in turn clears all session variables that the cart uses.
-session_destroy();
-
->>>>>>> 6a7d214d69340e5c2a1de5941f185dacd8210da5
 header("Location: index.php");
 ?>
